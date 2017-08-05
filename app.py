173a023660550fc315e68347a103981d89ca0e0e
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-04 23:08:24 -0600

    start it and get the verification done

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request,redirect,url_for
+import requests
+from random import choice
+from string import ascii_lowercase
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate')
+def generate():
+    code=''.join(choice(ascii_lowercase) for i in range(12))
+    return code
+
+@app.route('/checkurl', methods=["POST"])
+def check():
+    link = request.form
+    try:
+        webchecking = requests.get(link["input_web"],verify=False)
+
+        
+    except:
+        return "not a website"
+
+    if webchecking.status_code == 200:
+        return "yes"
+    else:
+        return "no"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
