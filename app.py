cd9ff6641f4efe16b64cca1131a30a70caceaf18
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-08 19:49:58 -0600

    hh

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 import requests
 import hashlib
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 
