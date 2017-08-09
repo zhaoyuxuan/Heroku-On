546e1bf1d17a991e10e71ba12caf3702d5bf8a62
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-08 20:20:03 -0600

    aa

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 db = SQLAlchemy(app)
 
 class Website(db.Model):
