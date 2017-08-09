f068b12b4c8311a2fea49c635ed9964f339c7258
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-08 20:30:13 -0600

    aa

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+
+
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Website(db.Model):
