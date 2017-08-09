1882d28fecedaa526320c7486d05d9e4c415b8c7
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-08 19:32:06 -0600

    add DB

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask,render_template,request,redirect,url_for
 import requests
 import hashlib
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
