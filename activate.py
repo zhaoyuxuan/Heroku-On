a1a8c9975302b937e76f5d31041ffbaed9060640
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-08-10 20:24:06 -0600

    s

--- activate.py
+++ activate.py
@@ -1,4 +1,21 @@
-import app.py
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+cursor=conn.cursor()
+cursor.execute("SELECT url FROM WEBSITE")
+urls=cursor.fetchall()
 
 def activate_website():
     rs = (grequests.get(u) for each in urls)
