5c604e026e66bf8652a148bedc6e4961fcf69eaa
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-09-04 00:04:32 -0600

    fix the spacing again

--- app.py
+++ app.py
@@ -8,13 +8,11 @@ import validators
 import psycopg2
 import sys,json
 
-
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 class WEBSITE(db.Model):
     __tablename__ = 'WEBSITE'
-
     url = db.Column("url",db.Text, primary_key=True)
 
     def __init__(self, url):
@@ -28,17 +26,16 @@ db.create_all()
 def index():
     return render_template('index.html')
 
-
 @app.route('/generate',methods=["POST"])
 def generate():
     link = request.form["input_url"]
-    result=validators.url(link, public=False)
+    result = validators.url(link, public=False)
     if not result:
         return "not a website"
     else :
-        link=link.encode('utf-8')
+        link = link.encode('utf-8')
         hash_object = hashlib.md5(link)
-        code=hash_object.hexdigest()
+        code = hash_object.hexdigest()
         return code[:5]
 
 @app.route('/04122')
@@ -60,29 +57,35 @@ def test():
 
 @app.route('/checkurl', methods=["POST"])
 def check():
-    store_link=[]
+    store_link = [] 
     link = request.form["website_url"]
+
     try:
-        webchecking = requests.get(link,verify=False)
+
+        check = requests.get(link,verify=False)
+
     except:
+        
         return "it is not in the website"
-    webcode=webchecking.text
-    url=request.form["website"]
+    webcode = check.text
+    url = request.form["website"]
     if (webcode in link):
+
         try:
-            data=WEBSITE(url)
+
+            data = WEBSITE(url)
             db.session.add(data)
             db.session.commit()
-            all_users = WEBSITE.query.all()
+        
         except:
-            db.session.rollback()
-            all_users = WEBSITE.query.all()
 
+            db.session.rollback()
+            
+        all_users = WEBSITE.query.all()
         db.session.close()
         return "it is in the website"
     else:
         return "it is not in the website"
-
 
 if __name__ == "__main__":
     app.run()
