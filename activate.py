ff453ee1ff0cb797a8477b5894df9629a9be7fa3
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-09-03 23:50:58 -0600

    fix the spacing

--- activate.py
+++ activate.py
@@ -1,21 +1,4 @@
 import grequests,json,requests
 from app import test
 
-# def activate():
-#     urls = ["http://urls.yuxuanz.com/test",]
-#
-#     request = requests.get("http://urls.yuxuanz.com/test")
-#
-#     print(request.text)
-#
-#     # for r in responses:
-#     #     print (json.loads(r))
-#
-#     # links=grequests.get("http://urls.yuxuanz.com/test")
-#     # print(links.json())
-#     # data  = grequests.map(links)
-#     # print(data)
-
-
-
 test()
